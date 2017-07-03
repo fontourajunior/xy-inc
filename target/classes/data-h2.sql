58a607d95528013953731826b91a0634d79bcffd
Antonio F Fontoura Júnior <douglas@morpheus.com>
2017-07-03 19:19:22 -0300

    remoção arquivos

--- target/classes/data-h2.sql
+++
@@ -1,7 +0,0 @@
-insert into tb_points_interest (name, coordinated_x, coordinated_y) values ('Lanchonete', 27, 12);
-insert into tb_points_interest (name, coordinated_x, coordinated_y) values ('Posto', 31, 18);
-insert into tb_points_interest (name, coordinated_x, coordinated_y) values ('Joalheria', 15, 12);
-insert into tb_points_interest (name, coordinated_x, coordinated_y) values ('Floricultura', 19, 21);
-insert into tb_points_interest (name, coordinated_x, coordinated_y) values ('Pub', 12, 8);
-insert into tb_points_interest (name, coordinated_x, coordinated_y) values ('Supermercado', 23, 6);
-insert into tb_points_interest (name, coordinated_x, coordinated_y) values ('Churrascaria', 28, 2);
